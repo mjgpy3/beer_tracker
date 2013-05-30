2c5e1eb39557e714282a1b8964e35573afeb08f5
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-30 00:35:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Beer Tracker
+-----
+
+A simple web site for tracking what beers you have tried. Hopefully, will reside on Heroku someday.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerTracker::Application.config.secret_token = '243d7d1ba80ae6668d04f95465d3dfd1b66b6d42348780087ced0ed1675592efec9be155facf810f81b500743e611f8ff5b8c8c6dcbce74a6c94a2310678eeba'
