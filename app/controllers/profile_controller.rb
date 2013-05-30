a4dadde45d12db52f7796c060db6fab731afc038
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-30 18:56:58 -0400

    The profile says the user's name

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,5 +3,13 @@ class ProfileController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @user = get_username_without_email current_user
+  end
+
+private
+
+  def get_username_without_email user
+    at_sign_location = user.email.index("@")
+    user.email[0..at_sign_location-1]
   end
 end
