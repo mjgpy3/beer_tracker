b0cff3de6f63b9327f22d8aa966099e9ba27fd06
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-30 00:44:06 -0400

    Did what devise install asked

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module BeerTracker
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Yes, we would like to deploy to Heroku at some time
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
