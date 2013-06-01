b0a447e5fbec85e16b2a0778d0896df6ac96d72b
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-31 20:55:34 -0400

    Still gotta reset, missed an add

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ BeerTracker::Application.routes.draw do
   resources :beers
 
   get "profile/index"
+  get "profile/add_tried_from_list"
+  get "profile/add_tried_by_new"
 
   root :to => "profile#index"
 end
