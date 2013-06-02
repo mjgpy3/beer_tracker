c3ab851082e310261746ab56bf0c6fc02d1a56bc
Michael J. Gilliland <mjg.py3@gmail.com>
2013-06-02 12:27:26 -0400

    Move to commit

--- app/models/tried.rb
+++ app/models/tried.rb
@@ -5,4 +5,14 @@ class Tried < ActiveRecord::Base
   def get_beer
     Beer.find(self.beer_id)
   end
+
+  def self.create_from_ids_and_rating user_id, beer_id, rating
+    tried = Tried.new
+
+    tried.user_id = user_id
+    tried.beer_id = beer_id
+    tried.rating = rating
+
+    tried.save
+  end
 end
